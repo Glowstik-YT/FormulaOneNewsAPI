294ceb4157fed697dcd995e8b6bcece3d1616351
Glowstik_ <69128866+Glowstik-YT@users.noreply.github.com>
2023-02-20 15:19:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -184,4 +184,4 @@ def update_cache(sleep_time: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
